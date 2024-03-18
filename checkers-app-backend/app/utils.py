57ccd852c2578568e56c1b74c7964d8edc818dcc
ancamach <andrea.cam.ag@gmail.com>
2024-03-18 11:41:20 +0100

    end game and promote to king functions added

--- checkers-app-backend/app/utils.py
+++ checkers-app-backend/app/utils.py
@@ -77,3 +77,51 @@ def is_same_color(to_position: str, player_color_id: int, db: Session = Depends(
     if piece_color_id == player_color_id:
         return True
     return False
+
+
+#Basic function to end the game, we could update it later if we need to
+def end_game(game_id: int, db: Session):
+    game = db.query(models.Game).filter(models.Game.id == game_id).first()
+    if game:
+        game.is_finished = True
+
+        db.commit()
+
+
+def promote_to_king(
+    piece_id: int, 
+    to_position: str,
+    player_id: int, 
+    game_id: int, 
+    db: Session
+) -> bool:
+
+    new_row = int(to_position.strip('{}').split(',')[0])
+
+    player = db.query(models.Player).filter(models.Player.id == player_id).first()
+    
+    if player and ((player.color_id == 1 and new_row == 7) or (player.color_id == 2 and new_row == 0)):
+        #I have called the move type promotion, if you had anything else in mind we can change it later
+        promotion_move_type = db.query(models.MoveType).filter(models.MoveType.name == "promotion").first()
+
+        last_move_order = db.query(models.Move).filter(models.Move.game_id == game_id).order_by(models.Move.move_order.desc()).first()
+        next_move_order = last_move_order.move_order + 1 if last_move_order else 1 
+
+        new_king_move = models.Move(
+            piece_id=piece_id,
+            game_id=game_id,
+            player_id=player_id,
+            move_type_id=promotion_move_type.id, #promotion name could change
+            move_order=next_move_order,
+            from_position=to_position,
+            to_position=to_position,
+            piece_taken='',
+            is_king=True
+        )
+        db.add(new_king_move)
+
+        end_game(game_id, player_id, db)
+        db.commit()
+        return True
+
+    return False
